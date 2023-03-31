9d4697946e36c07fa0dd9fe09a164ff3cd4c8394
filterbotu <128911514+filterbotu@users.noreply.github.com>
2023-03-31 17:39:42 +0530

    Update info.py

--- info.py
+++ info.py
@@ -1,1 +1,2 @@
+from os import environ
 IMDB_TEMPLATE = environ.get("IMDB_TEMPLATE", "<b>Query: {query}</b> \n‌IMDb Data:\n\n🏷 Title: <a href={url}>{title}</a>\n🎭 Genres: {genres}\n📆 Year: <a href={url}/releaseinfo>{year}</a>\n🌟 Rating: <a href={url}/ratings>{rating}</a> / 10")
